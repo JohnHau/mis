e7ee379bc14fbd6facab9016791037120da97393
Mike <jackyhau1024@163.com>
2020-02-23 22:49:48 +0800

    add read_celsius.c  read/write celsius form dht11  select() api

---
+++ read_celsius.c
@@ -0,0 +1,169 @@
+#include<sys/types.h>
+#include<errno.h>
+#include<string.h>
+#include<termios.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+#include <termios.h>
+
+//#define BLEN  128 
+#define BLEN  BUFSIZ 
+
+int main(int argc, char** argv)
+{
+	int fd;
+	int wfd;
+	int rfd;
+
+	
+	fd_set fds;
+	struct timeval tv;
+	int retval;
+
+
+	int nread,i;
+	int tnread;
+	int nwrite=0;
+	int cnt=0;
+	int s=0;
+	int tlen=0;
+
+	char wbuff[64] = "Hello\n";
+	uint8_t recbuf[BLEN]={0};
+	int cnt_rec=0;
+	int c;
+
+	char rbuff[BLEN] = {0};
+
+	tv.tv_sec =1;
+	tv.tv_usec = 500000;
+
+//	fd = open("/dev/ttyUSB0",O_RDWR|O_NOCTTY|O_NDELAY);
+//	fd = open("/dev/ttyUSB0",O_RDWR|O_NOCTTY);
+	//fd = open("/dev/ttyUSB0",O_RDONLY);
+//	fd = open("/dev/ttyUSB0",O_RDWR);
+//	fd = open("/dev/ttyS8",O_RDWR);
+	fd = open("/dev/ttyS4",O_RDWR);
+
+//	wfd = open("acom.log",O_WRONLY);
+//	rfd = open(argv[1],O_RDONLY);
+
+	if(fd == -1)
+	{	
+		perror("can not open serial port failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("fd is %d\n",fd);
+
+	//tcflush(fd,TCIFLUSH);
+	tcflush(fd,TCIOFLUSH);
+
+	cnt_rec=0;
+
+	char cmd[128] = "dht11";
+
+	//tlen = write(fd,cmd,6);
+#if 0
+	tlen = write(fd,"d",1);
+	tlen = write(fd,"h",1);
+	tlen = write(fd,"t",1);
+	tlen = write(fd,"1",1);
+	tlen = write(fd,"1",1);
+	//sleep(1);
+#endif
+	printf("tx:the amount is %d\n",tlen);
+
+	while(1)
+	{
+
+		FD_ZERO(&fds);
+		FD_SET(fd,&fds);
+		
+		//sleep(1);
+
+		retval = select(fd+1,&fds,NULL,NULL,&tv);
+		//retval = select(fd+1,&fds,NULL,NULL,NULL);
+
+		if(retval == -1)
+		{
+			printf("error:select\n");
+			close(fd);
+			exit(EXIT_FAILURE);
+
+		}
+		else if(retval)
+		{
+
+			memset(recbuf,0,sizeof(recbuf));
+			tlen =read(fd,recbuf,BLEN);	
+			//tlen =read(fd,recbuf,8);	
+#if 0
+			printf("rx:the amount is %d\n",tlen);
+			printf("%d\n",recbuf[0]);
+			printf("tlen is %d\n",tlen);
+#endif
+			printf("message is %s\n",recbuf);
+			//printf("msg is %c %c %c %c\n",recbuf[0],recbuf[1],recbuf[2],recbuf[3]);
+
+
+		}
+#if 1
+		else
+		{
+			
+			tlen = write(fd,cmd,6);
+
+			//printf("tlen is %d\n",tlen);
+
+			//exit(EXIT_FAILURE);
+		}
+#endif
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+	while((nread =read(rfd,recbuf,BLEN)) > 0)	
+	{
+
+		nwrite= write(fd,recbuf,nread);
+		printf("nwrite is %d\n",nwrite);
+
+		tnread=0;
+		while(tnread != nwrite)
+		{
+
+			printf("---tnread is %d---\n",tnread);
+
+			nread = read(fd,rbuff,BLEN);
+			tnread +=nread;
+			printf("---nread is %d---\n",nread);
+			write(wfd,rbuff,nread);
+		}
+
+
+	}
+
+	close(wfd);
+	close(rfd);
+	close(fd);
+#endif
+	printf("done\n");
+	return 0;
+}
